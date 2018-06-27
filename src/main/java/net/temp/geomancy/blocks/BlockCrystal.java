44769ade1787c820763cd24c78f1278f341bcc45
Niedzwiedz1 <michalski.michal-96@wp.pl>
2018-06-27 17:03:42 +0200

    trans

--- src/main/java/net/temp/geomancy/blocks/BlockCrystal.java
+++ src/main/java/net/temp/geomancy/blocks/BlockCrystal.java
@@ -3,25 +3,38 @@ package net.temp.geomancy.blocks;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockCrystal extends BlockBase {
 
 	public BlockCrystal() {
-		super(Material.ROCK, "crystal");
+		super(Material.GLASS, "crystal");
 	
 		
 	}
+
+	@SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.TRANSLUCENT;
+    }
 	
 	@Override
-	@Deprecated
 	public boolean isOpaqueCube(IBlockState state) {
 		return false;
 	}
 	
 	@Override
-	@Deprecated
 	public boolean isFullCube(IBlockState state) {
 		return false;
 	}
+	
+	public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+        return EnumBlockRenderType.MODEL;
+    }
 	
 }
