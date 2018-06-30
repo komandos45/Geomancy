da512acc52e90622cded3c034177a6bf80f7549d
Niedzwiedz1 <michalski.michal-96@wp.pl>
2018-07-01 00:51:06 +0200

    restored ore gen

--- src/main/java/net/temp/geomancy/world/ModWorldStoneGeneration.java
+++ src/main/java/net/temp/geomancy/world/ModWorldStoneGeneration.java
@@ -17,20 +17,20 @@ import net.temp.geomancy.blocks.ModBlocks;
 
 import java.util.Random;
 
-/*
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-*/
+
 
 import akka.japi.Predicate;
 
 public class ModWorldStoneGeneration implements IWorldGenerator {
 	
-	/*
+	
 	public static final Logger logger = LogManager.getLogger(GeomancyMod.modId);
 	public String debugMsg;
-	*/
 	
+
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 		if (world.provider.getDimension() == 0) { // the overworld
@@ -55,15 +55,15 @@ public class ModWorldStoneGeneration implements IWorldGenerator {
 		
 		d=(int) (2*Math.ceil(Math.pow(oreCount, 1/3)));
 		for (int i=0;i<d;i++){
-			ang=(i/d*Math.PI);
-			w = (int) Math.ceil(d/2*Math.cos(ang));
+			w = (int) Math.ceil(d*0.5*Math.cos(i/d*Math.PI));
 			for (int j=0;j<2*w;j++){
 				for (int k=0;k<2*w;k++) {
-					if (d/2<Math.pow((j-w)^2+(k-w)^2,0.5)){
-						pos = new BlockPos(basePos.getX()+i-w,basePos.getY()+j-w,basePos.getZ()+k-w);
-						if(world.getBlockState(pos).getBlock()==ModBlocks.geoStone || world.getBlockState(pos).getBlock()==Blocks.AIR){
+					if (d/2<Math.pow(Math.pow((j-w),2)+Math.pow((k-w),2),0.5)){
+						pos = new BlockPos(basePos.getX()+i,basePos.getY()+j-w,basePos.getZ()+k-w);
+						if(world.getBlockState(pos).getBlock()==ModBlocks.geoStone || world.getBlockState(pos).getBlock()==Blocks.AIR || world.getBlockState(pos).getBlock() == Blocks.STONE){
 							world.setBlockState(pos, crystal);
 							generated++;
+							//logger.debug("x: "+pos.getX()+" ,y: "+pos.getY()+" ,z: "+pos.getZ());
 							if (generated==oreCount){
 								return;
 							}
@@ -90,18 +90,20 @@ public class ModWorldStoneGeneration implements IWorldGenerator {
 			 */
 			
 			generator.generate(world, random, pos);
+			/*
 			switch (random.nextInt(5)){
 				case 0:
-					generateCrystal(pos, ModBlocks.oreBlackGem.getDefaultState(), world, 4, 3, random);
+					generateCrystal(pos, ModBlocks.oreBlackGem.getDefaultState(), world, 10, 3, random);
 				case 1:
-					generateCrystal(pos, ModBlocks.oreBlueGem.getDefaultState(), world, 4, 3, random);
+					generateCrystal(pos, ModBlocks.oreBlueGem.getDefaultState(), world, 10, 3, random);
 				case 2:
-					generateCrystal(pos, ModBlocks.oreYellowGem.getDefaultState(), world, 4, 3, random);
+					generateCrystal(pos, ModBlocks.oreYellowGem.getDefaultState(), world, 10, 3, random);
 				case 3:
-					generateCrystal(pos, ModBlocks.oreRedGem.getDefaultState(), world, 4, 3, random);
+					generateCrystal(pos, ModBlocks.oreRedGem.getDefaultState(), world, 10, 3, random);
 				case 4:
-					generateCrystal(pos, ModBlocks.oreGreenGem.getDefaultState(), world, 4, 3, random);
+					generateCrystal(pos, ModBlocks.oreGreenGem.getDefaultState(), world, 10, 3, random);
 			}
+			*/
 			
 		}
 	}
